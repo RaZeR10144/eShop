e25c427d9468e8b77fe942f9d90c0352a7811fc0
Vaibhav Mane <vaibhavmane101@gmail.com>
2017-07-16 16:26:43 +0530

    gaming project setup complete upload...

---
+++ pcgamingbackend/src/main/java/GameShop/pcgamingbackend/App.java
@@ -0,0 +1,13 @@
+package GameShop.pcgamingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
